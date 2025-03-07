febc020dd5630a2d80afa4b9ac68a40182da9e08
Saurabh <jsaurabh1906@gmail.com>
2025-03-07 20:14:56 +0530

    feat(auth-backend): Handling Cors

--- MERN-Authentication/backend/index.js
+++ MERN-Authentication/backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
@@ -7,6 +8,9 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Handling cors issue
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json()); // allows us to parse incoming request bodies
 app.use(cookieParser()); // allows us to parse incoming  cookies
